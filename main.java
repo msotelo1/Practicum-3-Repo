b8572cb589e9910a470eae73210a466863174ce7
Chuck476 <cdierbach@towson.edu>
2024-01-26 10:14:52 -0500

    Update main.java

--- main.java
+++ main.java
@@ -1,1 +1,15 @@
+// Identifier Naming in Java
 
+public class Main {
+  
+  public static void main(String[] args) {
+    int 1st_num = 1;
+    int last_num = 10;
+    int Sum = 0;
+
+    for(int i = 1st_num; i <= last_num; i++)
+      sum = sum + i;
+  
+    System.out.println("Sum = " + sum);
+  }
+}
