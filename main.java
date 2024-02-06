97317ff1e7b430505bb77a099fc07f6f1ef5e70b
msotelo1 <158190815+msotelo1@users.noreply.github.com>
2024-02-06 02:26:53 -0500

    Update main.java

--- main.java
+++ main.java
@@ -1,13 +1,15 @@
 // Identifier Naming in Java
 
-public class Main {
+public class Main 
+{
   
-  public static void main(String[] args) {
-    int 1st_num = 1;
+  public static void main(String[] args) 
+  {
+    int first_num = 1;
     int last_num = 10;
-    int Sum = 0;
+    int sum = 0;
 
-    for(int i = 1st_num; i <= last_num; i++)
+    for(int i = first_num; i <= last_num; i++)
       sum = sum + i;
   
     System.out.println("Sum = " + sum);
